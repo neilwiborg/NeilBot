5b2abb84863476199522b01f9f22f6d52491ae94
Neil Wiborg <NeilWiborg@outlook.com>
2022-11-22 00:49:14 -0800

    add basic music playing

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM python:3.10
 WORKDIR /bot
 COPY requirements.txt /bot/
 RUN pip install -r requirements.txt
+RUN apt-get update -qq && apt-get install ffmpeg -y
 COPY . /bot
 CMD python neilbot/bot.py
--- neilbot/cogs/player.py
+++ neilbot/cogs/player.py
@@ -3,61 +3,104 @@ import discord
 from discord.ext import commands
 import yt_dlp
 
+
 class Player(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
-	
-	@discord.slash_command(name = "join", description = "Have NeilBot join your voice channel")
-	@commands.cooldown(1, 10, commands.BucketType.user)
-	async def join_voice(self, ctx):
-		# give us 15 minutes instead of 3 seconds to respond
-		await ctx.defer(ephemeral = False)
+    def __init__(self, bot):
+        self.bot = bot
 
-		# check if the member is connected to a voice channel
-		if ctx.author.voice and ctx.author.voice.channel:
-			# get the voice channel the member is connected to
-			channel = ctx.author.voice.channel
-			try:
-				# attempt to connect to the same voice channel as the member
-				await channel.connect()
+    async def _getVoiceChannel(self, voice_channels):
+        # iterate through every voice channel on the server
+        for vc in voice_channels:
+            # iterate through every member currently connected to the voice channel
+            for m in vc.members:
+                # check if the member is the bot
+                if m == self.bot.user:
+                    # set the status so we know the bot was found and stop looking
+                    return vc
+        return None
 
-				await ctx.respond(f"Connected to {channel.name}")
-			except discord.ClientException:
-				await ctx.respond(f"Already connected to {channel.name}!")
-			except (asyncio.TimeoutError, discord.opus.OpusNotLoaded):
-				await ctx.respond(f"Unable to connect to {channel.name}, please try again later")
-		else:
-			await ctx.respond("Please join a choice channel first!")
-	
-	@discord.slash_command(name = "leave", description = "Have NeilBot leave your voice channel")
-	@commands.cooldown(1, 10, commands.BucketType.user)
-	async def leave_voice(self, ctx):
-		# give us 15 minutes instead of 3 seconds to respond
-		await ctx.defer(ephemeral = False)
+    @discord.slash_command(name="join", description="Have NeilBot join your voice channel")
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def join_voice(self, ctx):
+        # give us 15 minutes instead of 3 seconds to respond
+        await ctx.defer(ephemeral=False)
 
-		# get the server
-		server = ctx.guild
-		# get all voice channels on the server
-		voice_channels = server.voice_channels
+        # check if the member is connected to a voice channel
+        if ctx.author.voice and ctx.author.voice.channel:
+            # get the voice channel the member is connected to
+            channel = ctx.author.voice.channel
+            try:
+                # attempt to connect to the same voice channel as the member
+                await channel.connect()
 
-		foundVoiceChannel = None # whether or not we found the bot in a voice channel
-		# iterate through every voice channel on the server
-		for vc in voice_channels:
-			# iterate through every member currently connected to the voice channel
-			for m in vc.members:
-				# check if the member is the bot
-				if m == self.bot.user:
-					# set the status so we know the bot was found and stop looking
-					foundVoiceChannel = vc
-					break
-		
-		# if we found the bot in a voice channel
-		if foundVoiceChannel:
-			# disconnect the bot in this server
-			await server.voice_client.disconnect()
-			await ctx.respond(f"Disconnected from {foundVoiceChannel.name}")
-		else:
-			await ctx.respond("Error: not connected to voice channel")	
+                await ctx.respond(f"Connected to {channel.name}")
+            except discord.ClientException:
+                await ctx.respond(f"Already connected to {channel.name}!")
+            except (asyncio.TimeoutError, discord.opus.OpusNotLoaded):
+                await ctx.respond(f"Unable to connect to {channel.name}, please try again later")
+        else:
+            await ctx.respond("Please join a voice channel first!")
+
+    @discord.slash_command(name="leave", description="Have NeilBot leave your voice channel")
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def leave_voice(self, ctx):
+        # give us 15 minutes instead of 3 seconds to respond
+        await ctx.defer(ephemeral=False)
+
+        # get the server
+        server = ctx.guild
+        # get all voice channels on the server
+        voice_channels = server.voice_channels
+
+        # the voice channel we found the bot in
+        botVoiceChannel = await self._getVoiceChannel(voice_channels)
+
+        # if we found the bot in a voice channel
+        if botVoiceChannel:
+            # disconnect the bot in this server
+            await server.voice_client.disconnect()
+            await ctx.respond(f"Disconnected from {botVoiceChannel.name}")
+        else:
+            await ctx.respond("Error: not connected to voice channel")
+
+    @discord.slash_command(name="play", description="Play the YouTube video url")
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def play_audio(self, ctx, url):
+        YDL_OPTIONS = {
+            'format': 'bestaudio',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'outtmpl': 'song.%(ext)s',
+        }
+
+        # give us 15 minutes instead of 3 seconds to respond
+        await ctx.defer(ephemeral=False)
+
+        # get the server
+        server = ctx.guild
+        # get all voice channels on the server
+        voice_channels = server.voice_channels
+
+        # the voice channel we found the bot in
+        botVoiceChannel = self._getVoiceChannel(voice_channels)
+
+        if botVoiceChannel:
+            with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+                ydl.download(url)
+            voice_client = discord.utils.get(
+                self.bot.voice_clients, guild=server)
+            if not voice_client.is_playing():
+                voice_client.play(discord.FFmpegPCMAudio("song.mp3"))
+                voice_client.is_playing()
+                await ctx.respond(f"Now playing {url}")
+            else:
+                await ctx.respond("Already playing song")
+        else:
+            ctx.respond("Bot not in a voice channel!")
+
 
 def setup(bot):
-	bot.add_cog(Player(bot))
+    bot.add_cog(Player(bot))
