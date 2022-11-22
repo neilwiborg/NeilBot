207ff0b83efd68234c4ebfe07d71f43197328b1b
Neil Wiborg <NeilWiborg@outlook.com>
2022-11-22 14:45:47 -0800

    add additional player commands

--- neilbot/cogs/player.py
+++ neilbot/cogs/player.py
@@ -57,6 +57,15 @@ class Player(commands.Cog):
 
         # if we found the bot in a voice channel
         if botVoiceChannel:
+            # get the server voice client
+            voice_client = discord.utils.get(
+                self.bot.voice_clients, guild=server)
+            # check if a song is playing
+            if voice_client.is_playing():
+				# stop the audio
+                voice_client.stop()
+				# discord won't recognize the audio has stopped unless we wait before disconnecting
+                await asyncio.sleep(1)
             # disconnect the bot in this server
             await server.voice_client.disconnect()
             await ctx.respond(f"Disconnected from {botVoiceChannel.name}")
@@ -66,7 +75,7 @@ class Player(commands.Cog):
     @discord.slash_command(name="play", description="Play the YouTube video url")
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def play_audio(self, ctx, url):
-		# setup options for YouTube downloader
+        # setup options for YouTube downloader
         YDL_OPTIONS = {
             'format': 'bestaudio',
             'postprocessors': [{
@@ -88,23 +97,110 @@ class Player(commands.Cog):
         # the voice channel we found the bot in
         botVoiceChannel = await self._getVoiceChannel(voice_channels)
 
-		# only play music if the bot is in a voice channel
+        # only play music if the bot is in a voice channel
         if botVoiceChannel:
             with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
-				# download the song from the url
+                # download the song from the url
                 ydl.download(url)
-			# get the server voice client
+                # get the server voice client
             voice_client = discord.utils.get(
                 self.bot.voice_clients, guild=server)
-			# check if a song is already playing
+            # check if a song is already playing
             if not voice_client.is_playing():
-				# play the downloaded song
+                # play the downloaded song
                 voice_client.play(discord.FFmpegPCMAudio("song.mp3"))
                 await ctx.respond(f"Now playing {url}")
             else:
                 await ctx.respond("Already playing song")
         else:
-            ctx.respond("Bot not in a voice channel!")
+            await ctx.respond("Bot not in a voice channel!")
+
+    @discord.slash_command(name="stop", description="Stop the currently playing audio")
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def stop_audio(self, ctx):
+        # give us 15 minutes instead of 3 seconds to respond
+        await ctx.defer(ephemeral=False)
+
+        # get the server
+        server = ctx.guild
+        # get all voice channels on the server
+        voice_channels = server.voice_channels
+
+        # the voice channel we found the bot in
+        botVoiceChannel = await self._getVoiceChannel(voice_channels)
+
+        # only stop the music if the bot is in a voice channel
+        if botVoiceChannel:
+            # get the server voice client
+            voice_client = discord.utils.get(
+                self.bot.voice_clients, guild=server)
+            # check if a song is playing
+            if voice_client.is_playing():
+                # stop the audio
+                voice_client.stop()
+                await ctx.respond(f"Stopped playing audio")
+            else:
+                await ctx.respond("Error: no audio is playing")
+        else:
+            await ctx.respond("Bot not in a voice channel!")
+
+    @discord.slash_command(name="pause", description="Pause the currently playing audio")
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def pause_audio(self, ctx):
+        # give us 15 minutes instead of 3 seconds to respond
+        await ctx.defer(ephemeral=False)
+
+        # get the server
+        server = ctx.guild
+        # get all voice channels on the server
+        voice_channels = server.voice_channels
+
+        # the voice channel we found the bot in
+        botVoiceChannel = await self._getVoiceChannel(voice_channels)
+
+        # only pause the music if the bot is in a voice channel
+        if botVoiceChannel:
+            # get the server voice client
+            voice_client = discord.utils.get(
+                self.bot.voice_clients, guild=server)
+            # check if a song is playing
+            if voice_client.is_playing():
+                # pause the audio
+                voice_client.pause()
+                await ctx.respond(f"Paused audio. Use /resume to continue playing")
+            else:
+                await ctx.respond("Error: no audio is playing")
+        else:
+            await ctx.respond("Bot not in a voice channel!")
+
+    @discord.slash_command(name="resume", description="Resume the paused audio")
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def resume_audio(self, ctx):
+        # give us 15 minutes instead of 3 seconds to respond
+        await ctx.defer(ephemeral=False)
+
+        # get the server
+        server = ctx.guild
+        # get all voice channels on the server
+        voice_channels = server.voice_channels
+
+        # the voice channel we found the bot in
+        botVoiceChannel = await self._getVoiceChannel(voice_channels)
+
+        # only resume the music if the bot is in a voice channel
+        if botVoiceChannel:
+            # get the server voice client
+            voice_client = discord.utils.get(
+                self.bot.voice_clients, guild=server)
+            # check if a song is playing
+            if not voice_client.is_playing():
+                # resume the audio
+                voice_client.resume()
+                await ctx.respond(f"Resumed playing audio")
+            else:
+                await ctx.respond("Error: audio is already playing")
+        else:
+            await ctx.respond("Bot not in a voice channel!")
 
 
 def setup(bot):
